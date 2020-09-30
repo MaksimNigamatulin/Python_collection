88f226747cd4f56117eba1479f2d348168b97197
Maksim Nigamatulin <maksim.nigamatulin@ya.ru>
2020-10-01 00:06:06 +0300

    a new change in code

--- Unpacker/Unpacker.py
+++ Unpacker/Unpacker.py
@@ -11,17 +11,29 @@ import os
 import zipfile
 
 # каталог, в котором будем искать архивы
-zip_folder_path = r'C:\Users\User\Downloads'
+zip_folder_path = r'C:\Users\User\Downloads' # задать через inpyt()
 
-if not os.path.exists(r'C:\Users\User\Downloads\unpack files'):
-    # каталог, в который будем распаковывать архивы
-    unpack_folder_path = os.mkdir(r'C:\Users\User\Downloads\unpack files')
+# запрос пути для хранения распакованных данных
+unpacked_folder_path = str(input('Укажите каталог для распаковки архивов: '))
+
+# определяем какие архивы будут доступны для поиска
+zip_file = ['.zip', '.rar', '.7z', '.tar-gz']
+
+# проверка наличия каталога для хранения распакованных данных
+if not os.path.exists(unpacked_folder_path):
+    # если каталог отсутствует - создать его
+    storage_folder_path = os.mkdir(unpacked_folder_path)
+# поиск всех файлов в корневом каталоге
 for files in os.walk(zip_folder_path):
+    # выбор списка файлов
     for file in files:
+        # обход всех файлов в списке
         for item in file:
-            if item.lower().endswith('.zip')\
-                or item.lower().endswith('.rar'):
-                print(item)
+            # обход всех элементов из списка с типами архивов
+            for i in zip_file:
+                # сопоставление расширений найденных файлов и доступных типов архивов
+                if item.lower().endswith(i):
+                    print(item)
 
 
 #print(f'Архив {} успешно распакован.')
