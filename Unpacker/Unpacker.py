ac70df72d9f8762f473580c38e6cc473e5f9ec3c
Maksim Nigamatulin <maksim.nigamatulin@ya.ru>
2020-10-06 23:46:30 +0300

    Ver. 2.1

--- Unpacker/Unpacker.py
+++ Unpacker/Unpacker.py
@@ -1,9 +1,9 @@
-# coding: utf-8-sig
+
 # -----------------------------------#
 #       Maksim Nigamatulin           #
 #           29/09/2020               #
-#  Скрипт для распаковки архивов в   #
-#  заданный каталог.                 #
+# Скрипт для распаковки zip-архивов  #
+# в заданный каталог.                #
 # -----------------------------------#
 
 # импорт модулей
@@ -11,29 +11,41 @@ import os
 import zipfile
 
 # каталог, в котором будем искать архивы
-zip_folder_path = r'C:\Users\User\Downloads' # задать через inpyt()
-
+zip_folder_path = r'C:\Users\User\Downloads\Мои документы'  # задать через inpyt()
 # запрос пути для хранения распакованных данных
-unpacked_folder_path = str(input('Укажите каталог для распаковки архивов: '))
+unpacked_folder_path = r'C:\Users\User\Downloads\unpack files'  # str(input('Укажите каталог для распаковки архивов: '))
 
-# определяем какие архивы будут доступны для поиска
-zip_file = ['.zip', '.rar', '.7z', '.tar-gz']
+def unzip():
+    os.chdir(zip_folder_path)
+    # проверка наличия каталога для хранения распакованных данных
+    if not os.path.exists(unpacked_folder_path):
+        # если каталог отсутствует - создать его
+        os.mkdir(unpacked_folder_path)
+    # поиск всех файлов в корневом каталоге
+    for files in os.walk(zip_folder_path):
+        # выбор списка файлов
+        for file in files:
+            # обход всех файлов в списке
+            for item in file:
+                # выбор файлов по заданному расширению
+                if item.lower().endswith('.zip'):
+                    with zipfile.ZipFile(item, 'r') as zip_item:
+                        zip_item.extractall(unpacked_folder_path)
+                        print(f'Архив {item} успешно распакован.')
 
-# проверка наличия каталога для хранения распакованных данных
-if not os.path.exists(unpacked_folder_path):
-    # если каталог отсутствует - создать его
-    storage_folder_path = os.mkdir(unpacked_folder_path)
-# поиск всех файлов в корневом каталоге
-for files in os.walk(zip_folder_path):
-    # выбор списка файлов
-    for file in files:
-        # обход всех файлов в списке
-        for item in file:
-            # обход всех элементов из списка с типами архивов
-            for i in zip_file:
-                # сопоставление расширений найденных файлов и доступных типов архивов
-                if item.lower().endswith(i):
-                    print(item)
+def rename_folders_and_files(folder):
+    os.chdir(folder)
+    for dir in os.listdir():
+        if dir != dir.encode('cp437').decode('cp866'):
+            if os.path.isfile(dir):
+                decode_dirs = dir.encode('cp437').decode('cp866')
+                os.rename(dir, decode_dirs)
+            if os.path.isdir(dir):
+                decode_dirs = dir.encode('cp437').decode('cp866')
+                os.rename(dir, decode_dirs)
+                rename_folders_and_files(folder + '\\' + decode_dirs)
+                if not os.path.isdir(decode_dirs):
+                    rename_folders_and_files(folder)
 
-
-#print(f'Архив {} успешно распакован.')
+unzip()
+rename_folders_and_files(unpacked_folder_path)
